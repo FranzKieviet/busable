18a51140fc5717937d24c7257fb1c8ff7bbf4c1c
franzkieviet <franzkieviet@gmail.com>
2026-07-04 13:27:41 -0700

    fix bug in backend tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "franzkieviet-infra-terraform"
-    key            = "{var.project-name}/terraform.tfstate"
+    key            = "busable/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "franzkieviet-infra-terraform"
     encrypt        = true

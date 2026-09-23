ba28699b28f86d1f27fa488b480df5f9d3c37366
franzkieviet <franzkieviet@gmail.com>
2026-09-22 18:31:44 -0700

    Update eventbridge rule

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -1,6 +1,6 @@
-resource "aws_cloudwatch_event_rule" "files_uploaded_rule" {
+resource "aws_cloudwatch_event_rule" "agency_files_uploaded_rule" {
   name        = "${local.project_prefix}-gtfs-ingestion-trigger-rule"
-  description = "Trigger GTFS ingestion when files are uploaded to the S3 bucket"
+  description = "Trigger GTFS ingestion when files are uploaded to the S3 bucket  under triggers/gtfs/"
 
   event_pattern = jsonencode({
     source = ["aws.s3"]
@@ -11,7 +11,7 @@ resource "aws_cloudwatch_event_rule" "files_uploaded_rule" {
         }
         object = {
         key = [{
-            prefix = "triggers/"
+            prefix = "triggers/gtfs/"
         }]
         }
     }
@@ -19,7 +19,7 @@ resource "aws_cloudwatch_event_rule" "files_uploaded_rule" {
 }
 
 resource "aws_cloudwatch_event_target" "lambda" {
-  rule      = aws_cloudwatch_event_rule.files_uploaded_rule.name
+  rule      = aws_cloudwatch_event_rule.agency_files_uploaded_rule.name
   target_id = "invoke-lambda"
   arn       = aws_lambda_function.dataloader.arn
 }
@@ -29,13 +29,13 @@ resource "aws_lambda_permission" "allow_eventbridge" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.dataloader.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.files_uploaded_rule.arn
+  source_arn    = aws_cloudwatch_event_rule.agency_files_uploaded_rule.arn
 }
 
-// Additional rule for S3 uploads to the `imports/` prefix (places ingestion)
-resource "aws_cloudwatch_event_rule" "imports_files_uploaded_rule" {
-  name        = "${local.project_prefix}-gtfs-imports-ingestion-trigger-rule"
-  description = "Trigger ingestion when files are uploaded to the S3 bucket under imports/"
+// Rule for triggering places lambda
+resource "aws_cloudwatch_event_rule" "places_files_uploaded_rule" {
+  name        = "${local.project_prefix}-places-ingestion-trigger-rule"
+  description = "Trigger ingestion when files are uploaded to the S3 bucket under triggers/places/"
 
   event_pattern = jsonencode({
     source = ["aws.s3"]
@@ -45,22 +45,22 @@ resource "aws_cloudwatch_event_rule" "imports_files_uploaded_rule" {
         name = [aws_s3_bucket.ingestion_bucket.bucket]
       }
       object = {
-        key = [{ prefix = "imports/" }]
+        key = [{ prefix = "triggers/places/" }]
       }
     }
   })
 }
 
-resource "aws_cloudwatch_event_target" "lambda_imports" {
-  rule      = aws_cloudwatch_event_rule.imports_files_uploaded_rule.name
-  target_id = "invoke-lambda-imports"
+resource "aws_cloudwatch_event_target" "lambda_places" {
+  rule      = aws_cloudwatch_event_rule.places_files_uploaded_rule.name
+  target_id = "invoke-lambda-places"
   arn       = aws_lambda_function.dataloader.arn
 }
 
-resource "aws_lambda_permission" "allow_eventbridge_imports" {
-  statement_id  = "AllowExecutionFromEventBridgeImports"
+resource "aws_lambda_permission" "allow_eventbridge_places" {
+  statement_id  = "AllowExecutionFromEventBridgePlaces"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.dataloader.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.imports_files_uploaded_rule.arn
+  source_arn    = aws_cloudwatch_event_rule.places_files_uploaded_rule.arn
 }

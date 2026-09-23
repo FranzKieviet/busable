20d95cd73eec83eeaf58ab4c76709de29777c6f7
franzkieviet <franzkieviet@gmail.com>
2026-09-22 19:19:18 -0700

    Bug fix where files are not found

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,10 +2,11 @@
 resource "null_resource" "install_dependencies" {
   triggers = {
     requirements = filemd5("${path.module}/../dataloaders/requirements.txt")
+    dataloader_files = filemd5("${path.module}/../dataloaders/main.py")
   }
 
   provisioner "local-exec" {
-    command = "docker run --rm -v ${abspath("${path.module}/../dataloaders")}:/var/task public.ecr.aws/sam/build-python3.10:latest pip install -r requirements.txt -t ."
+    command = "docker run --rm -w /var/task -v ${abspath("${path.module}/../dataloaders")}:/var/task public.ecr.aws/sam/build-python3.10:latest bash -c 'pip install -r requirements.txt -t /var/task && find /var/task -type d -name __pycache__ -exec rm -rf {} + 2>/dev/null || true && find /var/task -type f -name \"*.dist-info\" -delete && find /var/task -type f -name \"*.pyc\" -delete && echo \"Dependencies installed and cleaned successfully\"'"
   }
 }
 
@@ -13,6 +14,16 @@ data "archive_file" "dataloader_lambda_zip" {
   type        = "zip"
   source_dir  = abspath("${path.module}/../dataloaders")
   output_path = "${path.module}/dataloader_lambda.zip"
+  
+  excludes = [
+    "*.git*",
+    "__pycache__",
+    "*.egg-info",
+    ".pytest_cache",
+    "data",
+    "Dockerfile",
+    ".env"
+  ]
 
   depends_on = [null_resource.install_dependencies]
 }
